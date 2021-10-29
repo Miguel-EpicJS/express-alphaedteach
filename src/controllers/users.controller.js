85cdaeba51516202c82474a2141e082db03ada9c
CodigoDeSenior <migueljnm840@gmail.com>
2021-10-29 06:25:54 -0300

    install nodemon, and add try catch to users.controller

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -2,17 +2,37 @@ const usersModel = require("../models/users.model");
 const bcrypt = require("bcrypt");
 
 exports.postUserAuthenticate = (req, res) => {
-    if ( bcrypt.compareSync(req.body.password, usersModel.getUsers()[req.body.username].password) ) {
-        res.cookie("session_id", req.body.username + "." + new Date().getTime(), {signed: true}).redirect("/dashboard");
+    try {
+        if ( bcrypt.compare(req.body.password, usersModel.getUsers()[req.body.username].password) ) {
+            res.cookie("session_id", req.body.username + "." + new Date().getTime(), {signed: true}).redirect("/dashboard");
+        }
+        else {
+            res.status(401).send("Your password or username is wrong");
+        }    
+    } catch (error) {
+        res.sendStatus(500);
     }
-    else {
-        res.sendStatus(401);
-    }
+
+    
 };
 
 exports.postUserNewAccount = (req, res) => {
-    const psHash = bcrypt.hashSync(req.body.password, 10);   
-    usersModel.addUser(req.body.username, {password: psHash})
-
-    res.cookie("session_id", req.body.username + "." + new Date().getTime(), {signed: true}).redirect("/dashboard");
+    try {
+        bcrypt.genSalt(13).then(salt => {
+            bcrypt.hash(req.body.password, salt).then(hash => {
+                console.log(usersModel.getUsers());
+                if (usersModel.getUsers()[req.body.username] === undefined) {
+                    usersModel.addUser(req.body.username, {password: hash})
+        
+                    res.cookie("session_id", req.body.username + "." + new Date().getTime(), {signed: true}).redirect("/dashboard");                        
+                }else{
+                    res.status(403).send("Use a different username");
+                }
+    
+            });    
+        });
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(500);
+    }    
 };
